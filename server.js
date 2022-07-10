99b2f3d867bea80c04de2fb0d96ea48d90eef110
Blain Thomas <85030084+BlainThomas@users.noreply.github.com>
2022-07-10 15:22:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'http://localhost:3001',
+  cors: '',
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
