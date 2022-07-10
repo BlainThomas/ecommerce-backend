619ae3568924ed897976346efef8f9824e4f902e
Blain Thomas <85030084+BlainThomas@users.noreply.github.com>
2022-07-10 15:37:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const http = require('http');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 require('./connection')
-// const server = http.createServer(app);
-// const {Server} = require('socket.io');
-// const io = new Server(server, {
-//   cors: 'http://localhost:3001',
-//   methods: ['GET', 'POST', 'PATCH', "DELETE"]
-// })
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
 
 
 const User = require('./models/User');
